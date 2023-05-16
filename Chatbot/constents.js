ab5552771d4026e3039095f88eddce39dcee4bbd
Cybug-Stuidos <113607291+Cybug-Stuidos@users.noreply.github.com>
2023-05-16 16:43:22 +0530

    Add files via upload

---
+++ Chatbot/constents.js
@@ -0,0 +1,102 @@
+/*const prompts=[
+["Hi","Hello","Hey","Good Morning","Good Evening","Hola","Nihoe"],
+["How are you?","How is your life?","Are you ok?"],
+["What's up?","What are you doing?","Do you need help?"],
+["My name is ChatBot","Yes, I am an AI","It is 4"],
+["No, because I am a AI, I ca not eat", "My birthday is April 24th 2023, so you tell me","No, because I am a AI, I ca not eat",],
+["Yes, looks very sweet","Yes, Minecraft is my favourite","Nobody knows, not even me"],
+["Nobody nose a joke","Mary had a little lamb, little lamb, little lamb"],
+["Look at the bottem-right of your screen","I am a AI in a website","Blue is my favourite colour"],
+["coolmathgamesoj.com","Yes, Grumbot is my father","No, I don't"],
+	]
+const replies=[
+["Hi","Hello There","Hello","How can I help?","Good Evening","Good Morning",],
+["I am fine","Good, How is yours?","Yes,I am ok"],
+["Nothing much, You?","I am doing just fine","No, thank you"],
+["What's your name","Your an AI?","Tell me what 2+2 is"],
+["Did you have lunch?","How old are you?","Did you have breakfast?"],
+["Do you like cake?","Do you like video games","How many stars are there in the sky?"],
+["Tell me a joke","sing a song","Write a poem"],
+["What is the time?","What are you?","What is your favourite colour?"],
+["Let's play a game","Do you have parents?","Do you have any brothers or sisters?"],
+	]
+const alternative=["Sorry, but I don't understand","I would say something to that, if I understood"
+
+	]
+const covid19=["Please stay at home","Wear a Mask","Be safe"]*/
+const prompts = [
+  ["hi", "hey", "hello", "good morning", "good afternoon"],
+  ["how are you", "how is life", "how are things"],
+  ["what are you doing", "what is going on", "what is up"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you"],
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what call yourself"
+  ],
+  ["i love you"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+  ["bad", "bored", "tired"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],
+  ["I am infinite"],
+  ["I am just a bot", "I am a bot. What are you?"],
+  ["The one true God, JavaScript"],
+  ["I am nameless", "I don't have a name"],
+  ["I love you too", "Me too"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why? You shouldn't!", "Try watching TV"],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"]
+]
+
+// Random for any other user input
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
+
